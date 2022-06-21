9b63e6799c8fe9d8e01a2a7ad18bc94ece0666e5
pavel-nikitin-2022 <90633463+pavel-nikitin-2022@users.noreply.github.com>
2022-06-22 01:48:17 +0300

    fixed regression.py
    delete 10% of bad code

--- Lab6/Lab6/regression.py
+++ Lab6/Lab6/regression.py
@@ -8,8 +8,8 @@ import random
 def regression_equation(data):
     X = data[:-1]
     Y = data[1:]
-    a = search_a(X, Y)
     b = search_b(X, Y)
+    a = search_a(X, Y, b)
     forecast = []
 
     for i in data:
@@ -24,30 +24,12 @@ def regression_equation(data):
     ax.plot(forecast);
     plt.show()
 
-#ковариация
-def cov(X, Y):
-    average_x = sum(X) / len(X)
-    average_y = sum(Y) / len(Y)
-    numenator = 0
-    for i in range(len(X)):
-        numenator += (Y[i] - average_y) * (X[i] - average_x)
-
-    return numenator / (len(X) - 1)
-
-#диссперсия
-def var(X, Y):
-    average_x = sum(X) / len(X)
-    numenator = 0
-    for i in range(len(X)):
-        numenator += math.pow((X[i] - average_x), 2)
-
-    return numenator / len(X) 
-
 #поиск угла пересечения с осью линейной регрессии
-def search_a(X, Y):
-    numerator = cov(X, Y)
-    denominator = var(X, Y)
-    return numerator / denominator
+def search_a(X, Y, b):
+    a = 0
+    for i in range(len(X)):
+        a += Y[i] - b * X[i]
+    return a / len(X)
 
 #поиск угла наклона линейной регрессии
 def search_b(X, Y):
@@ -57,7 +39,10 @@ def search_b(X, Y):
     denominator = 0
 
     for i in range(len(X)):
+        #ковариация
         numenator += (Y[i] - average_y) * (Y[i] - average_x)
+        #дисперсия
         denominator += math.pow((X[i] - average_x), 2)
 
     return numenator / denominator
+
